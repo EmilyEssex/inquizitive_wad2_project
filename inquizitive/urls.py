c84a22cb99f03e456eba1d672392aa122c08a71f
MohammedNader AlHaffar <2581031A@student.gla.ac.ukgit clone https://github.com/Naderalhaffar/tango_with_django_project.gitexitexit()git config --global user.name MohammedNader>
2022-03-02 21:45:12 +0000

    sign up, log in and logout

--- inquizitive/urls.py
+++ inquizitive/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from inquizitive import views
+from . import views
 
 
-app_name = 'inquizitive'
+#app_name = 'inquizitive'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('login/', views.login_user, name ='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
