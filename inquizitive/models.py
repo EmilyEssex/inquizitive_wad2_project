0b21f4f2feecf63bfc0b84d20ce51d22d2b01847
Apple <apple@apples-mbp-2.home>
2022-03-03 22:35:10 +0000

    March 3

--- inquizitive/models.py
+++ inquizitive/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.db import models
+from django.contrib import admin
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+import json
 
+
+def main():
+    mock_dict={"quizId" :["option1", "option2","option3"], "quizId" :["option1", "option2","option3"]}
+
+    json1=json.dumps(mock_dict)
+    print (json1)
+    
+if __name__ == "__main__":
+    main()
+
+
+ 
 # Create your models here.
+#RATING_OPTIONS= ("easy", "medium","hard")
+
+    
+class Answer(models.Model):
+    answer_text= models.CharField(max_length=500, null=True)
+   # question=models.ForeignKey(Question , on_delete=models.CASCADE)
+    correct_answer=models.BooleanField(default=False)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+ 
+    class Meta:
+        verbose_name_plural = 'answers'
+    def __str__(self):
+        return self.answer_text
+
+class Question(models.Model):
+    question_text= models.CharField(max_length=500, unique=True)
+    answer_options=models.ManyToManyField(Answer)
+    #slug = models.SlugField(unique=True)
+   # def save(self, *args, **kwargs):
+        #self.slug = slugify(self.text) 
+        #super(Question, self).save(*args, **kwargs)
+    class Meta:
+        verbose_name_plural = 'questions'
+    def __str__(self):
+        return self.question_text
+
+class Quiz(models.Model):
+    name= models.CharField(max_length=128, unique=True)
+    subject= models.CharField(max_length=128)
+    private =models.BooleanField(default=False)
+    question_text= models.JSONFIELD()
+    answer_text= models.JSONFIELD()
+    if private:
+        uniqueCode=models.CharField(max_length=128, unique=True)
+    # will be the average rating
+    rating = models.CharField(max_length=10)
+    quiz_questions=models.ManyToManyField(Question)
+    class Meta:
+        verbose_name_plural = 'quizzes'
+    def __str__(self):
+        return self.name
+
+    
+class Comment(models.model):
+     comment_text= models.CharField(max_length=500, null=True)
+     quiz=models.ForeignKey(Quiz, on_delete=models.CASCADE) 
+     user=models.ForeignKey(User, on_delete=models.CASCADE) 
+     #finalScore=models.FloatField(defualt=0)
+     class Meta:
+        verbose_name_plural = 'comments'
+     def __str__(self):
+        return self.comment_text
+    
+ 
--- inquizitive_wad2_project/settings.py
+++ inquizitive_wad2_project/settings.py
@@ -119,3 +119,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# new 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+ 
+STATIC_DIR = os.path.join(BASE_DIR, 'static') 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
+STATICFILES_DIRS = [STATIC_DIR, ] 
