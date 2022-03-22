572d05a6d6f60aacccaea8c690893331cc146ec1
Apple <apple@apples-mbp-2.home>
2022-03-22 19:58:36 +0000

    solved division by zero error

--- inquizitive/views.py
+++ inquizitive/views.py
@@ -279,47 +279,7 @@ def quizResults(request,quiz_name_slug):
      
             
             
-            
-'''          
-            
-        if request.method == 'POST':
-            quiz = Quiz.objects.get(slug=quiz_name_slug)
-            context_dict['quizName'] = quiz.quizName
-            questions = Question.objects.filter(quiz=quiz)
-            context_dict['questions'] = questions
-            context_dict['quiz'] = quiz
-            context_dict['numOfQue']= quiz.numOfQue
-            context_dict['optionsList']=[Question.optiona,Question.optionb,Question.optionc,Question.optiond]
-            score =0
-            maxPossibleScore=0
-            for q in questions:
-                maxPossibleScore+=q.questionMarks
-                print(request.POST.get(q.questionText))
-                print(q.correctAnswer)
-                print()
-                if q.correctAnswer ==  request.POST.get(q.questionText):
-                    score+=q.questionMarks
-                percent = score/(maxPossibleScore) *100
-                context2 = {
-                'score':score,
-                'percent':percent
-                }
-        return render(request,'inquizitive/quizResults.html',context2)
-      
-
-        
-     except Quiz.DoesNotExist:
  
-        context_dict['quiz'] = None
-        context_dict['questions'] = None
-    # Go render the response and return it to the client.
-
-     return render(request, 'inquizitive/quiz.html', context=context_dict)
-
-answer is associated with answerQuiz.html
-  
- ''' 
-
 # This view handles both allowing the user to answer and displaying the results
 # The above results view could be removed 
 def answerQuiz(request, quiz_name_slug):
@@ -340,8 +300,10 @@ def answerQuiz(request, quiz_name_slug):
             if question.correctAnswer ==  request.POST.get(question.questionText):
                 score+=question.questionMarks
                 
-            
-        percent = score/(total) *100
+        if total!=0:    
+            percent = score/(total) *100
+        else:
+            percent=0
         context = {
             'time': request.POST.get('timer'),
             'percent':percent
